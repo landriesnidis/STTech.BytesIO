ccc7ff8577b6212d994aa1a85b410d7735b7ce53
Guijie Lee <landriesnidis@yeah.net>
2022-07-12 00:16:04 +0800

    新增数据发送器接口扩展，对原有的发送器的同步方法提供异步的扩展

---
+++ STTech.BytesIO.Core/Extensions/DataSenderExtension.cs
@@ -0,0 +1,45 @@
+﻿using STTech.BytesIO.Core.Entity;
+using System.Threading.Tasks;
+
+namespace STTech.BytesIO.Core
+{
+    /// <summary>
+    /// 数据发送器接口扩展
+    /// 对原有的发送器的同步方法提供异步的扩展
+    /// </summary>
+    public static class DataSenderExtension
+    {
+        /// <summary>
+        /// 异步发送数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sender"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static Task SendAsync<T>(IDataSender<T> sender, T request)
+        {
+            return Task.Run(() => sender.Send(request));
+        }
+
+        /// <summary>
+        /// 异步数据
+        /// 异步线程等待单次发送的响应结果
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="timeout">超时时间(ms)</param>
+        /// <param name="matchHandler">收发数据匹配回调。
+        /// 每次接收到的数据帧不一定就是对上一条发送数据的响应（如心跳包等），
+        /// 所以需要根据协议编写对收发数据帧匹配的回调用以确定正确的响应数据。
+        /// 包括不限于以下方式：
+        /// 1.对发送数据的命令位于接受数据的命令位进行对比；
+        /// 2.对发送数据的任务号及通信计数与接收数据的对应位进行对比；
+        /// 3.过滤高频的主动推送数据（如：心跳包、状态更新、异常报告等）,取其后第一帧；
+        /// </param>
+        /// <returns>单次发送数据并等待远端响应的任务</returns>
+        public static Task<Reply<TRecv>> SendAsync<TSend, TRecv>(IDataSender<TSend, TRecv> sender, TSend data, int timeout, ReplyMatchHandler<TSend, TRecv> matchHandler = null)
+        {
+            return Task.Run(() => sender.Send(data, timeout, matchHandler));
+        }
+
+    }
+}
