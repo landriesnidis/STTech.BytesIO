dc0c361cbff260aeba24277cce6349dfbb3183c2
Guijie Lee <landriesnidis@yeah.net>
2022-08-26 22:04:59 +0800

    添加事件参数成员的注释

--- STTech.BytesIO.Tcp/Entity/TlsVerifySuccessfullyEventArgs.cs
+++ STTech.BytesIO.Tcp/Entity/TlsVerifySuccessfullyEventArgs.cs
@@ -8,12 +8,39 @@ namespace STTech.BytesIO.Tcp.Entity
 {
     public class TlsVerifySuccessfullyEventArgs : EventArgs
     {
+        /// <summary>
+        /// 批量加密算法
+        /// </summary>
         public CipherAlgorithmType CipherAlgorithm { get; }
+
+        /// <summary>
+        /// 密码算法的强度
+        /// </summary>
         public int CipherStrength { get; }
+
+        /// <summary>
+        /// 获取用于生成消息身份验证代码 (MAC) 的算法
+        /// </summary>
         public HashAlgorithmType HashAlgorithm { get; }
+
+        /// <summary>
+        /// 获取此实例使用的哈希算法的强度
+        /// </summary>
         public int HashStrength { get; }
+
+        /// <summary>
+        /// 密钥交换算法
+        /// </summary>
         public ExchangeAlgorithmType KeyExchangeAlgorithm { get; }
+
+        /// <summary>
+        /// 获此实例使用的密钥交换算法的强度
+        /// </summary>
         public int KeyExchangeStrength { get; }
+
+        /// <summary>
+        /// 获取此连接进行身份验证的安全协议
+        /// </summary>
         public SslProtocols SslProtocol { get; }
 
         public TlsVerifySuccessfullyEventArgs(SslStream stream)
