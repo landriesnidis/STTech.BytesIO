407c170039046163f3dcc982a99c64da56c241f7
Guijie Lee <landriesnidis@yeah.net>
2022-09-01 23:30:45 +0800

    更新注释，优化接口

--- STTech.BytesIO.Serial/ISerialClient.cs
+++ STTech.BytesIO.Serial/ISerialClient.cs
@@ -1,23 +1,158 @@
-﻿using System.IO.Ports;
+﻿using System.IO;
+using System.IO.Ports;
+using System.Runtime.Serialization;
 
 namespace STTech.BytesIO.Serial
 {
     public interface ISerialClient
     {
+        /// <summary>
+        /// 获取或设置用于通信的端口，包括但不限于所有可用的COM端口。
+        /// 通信端口默认为COM1。
+        /// </summary>
         string PortName { get; set; }
+
+        /// <summary>
+        /// 获取或设置串口波特率。
+        /// </summary>
         int BaudRate { get; set; }
+
+        /// <summary>
+        /// 获取或设置用于串口传输数据的握手协议
+        /// 默认为None。
+        /// </summary>
         Handshake Handshake { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值，该值在串行通信期间启用数据终端就绪(DTR)信号。
+        /// 使数据终端就绪(DTR);否则,假的。默认为false。
+        /// </summary>
         bool DtrEnable { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值，该值指示在串行通信期间是否启用发送请求(RTS)信号。
+        /// </summary>
         bool RtsEnable { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值，该值指示在端口和接收缓冲区之间传输时是否忽略空字节。
+        /// </summary>
         bool DiscardNull { get; set; }
+
+        /// <summary>
+        /// 获取或设置用于解释对System.IO.Ports.SerialPort.ReadLine和System.IO.Ports.SerialPort.WriteLine(System.String)方法调用结束的值。
+        /// 表示一行结束的值。默认是换行符(c#中的“\n”或VisualBasic中的Microsoft.VisualBasic.Constants.vbLf)。
+        /// </summary>
         string NewLine { get; set; }
+
+        /// <summary>
+        /// 获取或设置当读取操作未完成时发生超时之前的毫秒数。
+        /// </summary>
         int ReadTimeout { get; set; }
+
+        /// <summary>
+        /// 获取或设置当写操作未完成时发生超时之前的毫秒数。
+        /// 超时发生前的毫秒数。默认为System.IO.Ports.SerialPort.InfiniteTimeout。
+        /// </summary>
         int WriteTimeout { get; set; }
 
-        //int ReadBufferSize { get; set; }
-        //int WriteBufferSize { get; set; }
+        /// <summary>
+        /// 获取或设置输入缓冲区的大小。
+        /// 缓冲区大小以字节为单位,默认值是4096。
+        /// 最大值是一个正整数(2147483647)。
+        /// </summary>
+        int ReceiveBufferSize { get; set; }
 
+        /// <summary>
+        /// 获取或设置串行端口输出缓冲区的大小。
+        /// 输出缓冲区的大小默认为2048。
+        /// </summary>
+        int SendBufferSize { get; set; }
+
+        /// <summary>
+        /// 获取或设置中断信号状态。
+        /// 如果端口处于中断状态，则为True;否则为False。
+        /// </summary>
+        [IgnoreDataMember]
+        bool BreakState { get; set; }
+
+        /// <summary>
+        /// 获取或设置每个字节的标准停止位数。
+        /// </summary>
+        StopBits StopBits { get; set; }
+
+        /// <summary>
+        /// 获取或设置奇偶检查协议。
+        /// 默认为System.IO.Ports.Parity.None。
+        /// </summary>
+        Parity Parity { get; set; }
+
+        /// <summary>
+        /// 获取发送缓冲区中数据的字节数。
+        /// </summary>
+        [IgnoreDataMember]
+        int BytesToWrite { get; }
+
+        /// <summary>
+        /// 获取System.IO.Ports.SerialPort对象的底层System.IO.Stream对象。
+        /// </summary>
+        [IgnoreDataMember]/// <summary>
+        /// 获取或设置奇偶检查协议。
+        /// 默认为System.IO.Ports.Parity.None。
+        /// </summary>
+        Stream BaseStream { get; }
+
+        /// <summary>
+        /// 获取接收缓冲区中数据的字节数。
+        /// </summary>
+        [IgnoreDataMember]
+        int BytesToRead { get; }
+
+        /// <summary>
+        /// 获取或设置在System.IO.Ports.SerialPort.DataReceived事件发生之前内部输入缓冲区中的字节数。
+        /// 触发System.IO.Ports.SerialPort.DataReceived事件之前内部输入缓冲区中的字节数。缺省值是1。
+        /// </summary>
+
+        int ReceivedBytesThreshold { get; set; }
+
+        /// <summary>
+        /// 获取端口的Carrier Detect行状态。
+        /// </summary>
+        [IgnoreDataMember]
+        bool CDHolding { get; }
+
+        /// <summary>
+        /// 获取或设置发生奇偶校验错误时替换数据流中无效字节的字节。
+        /// </summary>
+        byte ParityReplace { get; set; }
+
+        /// <summary>
+        /// 获取数据集就绪(DSR)信号的状态。
+        /// </summary>
+        [IgnoreDataMember]
+        bool DsrHolding { get; }
+
+        /// <summary>
+        /// 获取或设置每个字节的数据位的标准长度。
+        /// 有效值范围：5~8
+        /// </summary>
+        int DataBits { get; set; }
+
+        /// <summary>
+        /// 获取清除到发送行的状态。
+        /// 如果检测到清除发送行，则为true;否则为False。
+        /// </summary>
+        [IgnoreDataMember]
+        bool CtsHolding { get; }
+
+        /// <summary>
+        /// 丢弃来自串行驱动程序的接收缓冲区的数据
+        /// </summary>
         void DiscardInBuffer();
+
+        /// <summary>
+        /// 丢弃来自串行驱动程序的传输缓冲区的数据
+        /// </summary>
         void DiscardOutBuffer();
     }
 }
--- STTech.BytesIO.Serial/SerialClient.cs
+++ STTech.BytesIO.Serial/SerialClient.cs
@@ -11,165 +11,26 @@ using System.Threading;
 
 namespace STTech.BytesIO.Serial
 {
-    public class SerialClient : BytesClient, ISerialClient
+    public partial class SerialClient : BytesClient
     {
         /// <summary>
         /// 串口通信对象
         /// </summary>
-        protected SerialPort innerClient = null;
-
-        /// <summary>
-        /// 获取或设置用于串口传输数据的握手协议
-        /// 默认为None。
-        /// </summary>
-        public Handshake Handshake { get => innerClient.Handshake; set => innerClient.Handshake = value; }
-
-        /// <summary>
-        /// 获取或设置文本传输前和传输后转换的字节编码。
-        /// 默认为System.Text.ASCIIEncoding。
-        /// </summary>
-        protected Encoding Encoding { get => innerClient.Encoding; set => innerClient.Encoding = value; }
-
-        /// <summary>
-        /// 获取或设置一个值，该值在串行通信期间启用数据终端就绪(DTR)信号。
-        /// 使数据终端就绪(DTR);否则,假的。默认为false。
-        /// </summary>
-        public bool DtrEnable { get => innerClient.DiscardNull; set => innerClient.DiscardNull = value; }
-
-        /// <summary>
-        /// 获取清除到发送行的状态。
-        /// 如果检测到清除发送行，则为true;否则为False。
-        /// </summary>
-        [IgnoreDataMember]
-        public bool CtsHolding => innerClient.CtsHolding;
-        /// <summary>
-        /// 获取或设置一个值，该值指示在端口和接收缓冲区之间传输时是否忽略空字节。
-        /// </summary>
-        public bool DiscardNull { get => innerClient.DiscardNull; set => innerClient.DiscardNull = value; }
-
-        /// <summary>
-        /// 获取或设置每个字节的数据位的标准长度。
-        /// 有效值范围：5~8
-        /// </summary>
-        public int DataBits { get => innerClient.DataBits; set => innerClient.DataBits = value; }
+        protected SerialPort InnerClient { get; set; }
 
         /// <summary>
         /// 获取一个指示打开或关闭状态的值
         /// </summary>
         [IgnoreDataMember]
-        public override bool IsConnected => innerClient.IsOpen;
-
-        /// <summary>
-        /// 获取数据集就绪(DSR)信号的状态。
-        /// </summary>
-        [IgnoreDataMember]
-        public bool DsrHolding => innerClient.DsrHolding;
-
-        /// <summary>
-        /// 获取或设置用于解释对System.IO.Ports.SerialPort.ReadLine和System.IO.Ports.SerialPort.WriteLine(System.String)方法调用结束的值。
-        /// 表示一行结束的值。默认是换行符(c#中的“\n”或VisualBasic中的Microsoft.VisualBasic.Constants.vbLf)。
-        /// </summary>
-        public string NewLine { get => innerClient.NewLine; set => innerClient.NewLine = value; }
-
-        /// <summary>
-        /// 获取或设置输入缓冲区的大小。
-        /// 缓冲区大小以字节为单位,默认值是4096。
-        /// 最大值是一个正整数(2147483647)。
-        /// </summary>
-        public override int ReceiveBufferSize { get => innerClient.ReadBufferSize; set => innerClient.ReadBufferSize = value; }
-
-        /// <summary>
-        /// 获取或设置发生奇偶校验错误时替换数据流中无效字节的字节。
-        /// </summary>
-        public byte ParityReplace { get => innerClient.ParityReplace; set => innerClient.ParityReplace = value; }
-
-        /// <summary>
-        /// 获取或设置用于通信的端口，包括但不限于所有可用的COM端口。
-        /// 通信端口默认为COM1。
-        /// </summary>
-        public string PortName { get => innerClient.PortName; set => innerClient.PortName = value; }
-
-        /// <summary>
-        /// 获取端口的Carrier Detect行状态。
-        /// </summary>
-        [IgnoreDataMember]
-        public bool CDHolding => innerClient.CDHolding;
-
-        /// <summary>
-        /// 获取或设置当读取操作未完成时发生超时之前的毫秒数。
-        /// </summary>
-        public int ReadTimeout { get => innerClient.ReadTimeout; set => innerClient.ReadTimeout = value; }
-
-        /// <summary>
-        /// 获取或设置在System.IO.Ports.SerialPort.DataReceived事件发生之前内部输入缓冲区中的字节数。
-        /// 触发System.IO.Ports.SerialPort.DataReceived事件之前内部输入缓冲区中的字节数。缺省值是1。
-        /// </summary>
-        public int ReceivedBytesThreshold { get; set; }
-        /// <summary>
-        /// 获取或设置一个值，该值指示在串行通信期间是否启用发送请求(RTS)信号。
-        /// </summary>
-        public bool RtsEnable { get => innerClient.RtsEnable; set => innerClient.RtsEnable = value; }
-
-        /// <summary>
-        /// 获取或设置每个字节的标准停止位数。
-        /// </summary>
-        public StopBits StopBits { get => innerClient.StopBits; set => innerClient.StopBits = value; }
-
-        /// <summary>
-        /// 获取或设置串行端口输出缓冲区的大小。
-        /// 输出缓冲区的大小默认为2048。
-        /// </summary>
-        public override int SendBufferSize { get => innerClient.WriteBufferSize; set => innerClient.WriteBufferSize = value; }
-
-        /// <summary>
-        /// 获取或设置当写操作未完成时发生超时之前的毫秒数。
-        /// 超时发生前的毫秒数。默认为System.IO.Ports.SerialPort.InfiniteTimeout。
-        /// </summary>
-        public int WriteTimeout { get => innerClient.WriteTimeout; set => innerClient.WriteTimeout = value; }
-
-        /// <summary>
-        /// 获取或设置奇偶检查协议。
-        /// 默认为System.IO.Ports.Parity.None。
-        /// </summary>
-        public Parity Parity { get => innerClient.Parity; set => innerClient.Parity = value; }
-
-        /// <summary>
-        /// 获取发送缓冲区中数据的字节数。
-        /// </summary>
-        [IgnoreDataMember]
-        public int BytesToWrite => innerClient.BytesToWrite;
-
-        /// <summary>
-        /// 获取或设置串口波特率。
-        /// </summary>
-        public int BaudRate { get => innerClient.BaudRate; set => innerClient.BaudRate = value; }
-
-        /// <summary>
-        /// 获取或设置中断信号状态。
-        /// 如果端口处于中断状态，则为True;否则为False。
-        /// </summary>
-        [IgnoreDataMember]
-        public bool BreakState { get => innerClient.BreakState; set => innerClient.BreakState = value; }
-
-        /// <summary>
-        /// 获取System.IO.Ports.SerialPort对象的底层System.IO.Stream对象。
-        /// </summary>
-        [IgnoreDataMember]
-        public Stream BaseStream => innerClient.BaseStream;
-
-        /// <summary>
-        /// 获取接收缓冲区中数据的字节数。
-        /// </summary>
-        [IgnoreDataMember]
-        public int BytesToRead => innerClient.BytesToRead;
+        public override bool IsConnected => InnerClient.IsOpen;
 
         public SerialClient()
         {
             // 初始化
-            innerClient = new SerialPort();
+            InnerClient = new SerialPort();
 
             // 添加数据接收回调事件
-            innerClient.DataReceived += InnerClient_DataReceived;
+            InnerClient.DataReceived += InnerClient_DataReceived;
         }
 
         /// <summary>
@@ -177,11 +38,11 @@ namespace STTech.BytesIO.Serial
         /// </summary>
         public override void Connect()
         {
-            if (innerClient.IsOpen) return;
+            if (InnerClient.IsOpen) return;
 
             try
             {
-                innerClient.Open();
+                InnerClient.Open();
 
                 // 执行连接成功回调事件
                 RaiseConnectedSuccessfully(this, new ConnectedSuccessfullyEventArgs());
@@ -202,14 +63,14 @@ namespace STTech.BytesIO.Serial
         /// </summary>
         public override void Disconnect(DisconnectionReasonCode code = DisconnectionReasonCode.Active, Exception ex = null)
         {
-            if (!innerClient.IsOpen) return;
+            if (!InnerClient.IsOpen) return;
 
             // 关闭异步任务
             // 注：SerialPort已提供了DataReceived事件，不需要再实现异步接收
             // CancelReceiveDataTask();
 
             // 关闭串口
-            innerClient.Close();
+            InnerClient.Close();
 
             // 执行通信已断开的回调事件 
             RaiseDisconnected(this, new DisconnectedEventArgs() { ReasonCode = DisconnectionReasonCode.Active });
@@ -228,7 +89,7 @@ namespace STTech.BytesIO.Serial
             {
                 // 获取数据
                 var bytes = new byte[len];
-                innerClient.Read(bytes, 0, len);
+                InnerClient.Read(bytes, 0, len);
 
                 // 更新时间戳
                 UpdateLastMessageTimestamp();
@@ -247,7 +108,7 @@ namespace STTech.BytesIO.Serial
             try
             {
                 // 发送数据
-                innerClient.Write(data, 0, data.Length);
+                InnerClient.Write(data, 0, data.Length);
 
                 // 执行数据已发送的回调事件
                 RaiseDataSent(this, new DataSentEventArgs(data));
@@ -270,21 +131,139 @@ namespace STTech.BytesIO.Serial
         protected override void ReceiveDataCompletedHandle() { }
 
         protected override void ReceiveDataHandle() { }
+    }
+
+    public partial class SerialClient : ISerialClient
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Handshake Handshake { get => InnerClient.Handshake; set => InnerClient.Handshake = value; }
 
         /// <summary>
-        /// 丢弃来自串行驱动程序的接收缓冲区的数据
+        /// <inheritdoc/>
+        /// </summary>
+        public bool DtrEnable { get => InnerClient.DiscardNull; set => InnerClient.DiscardNull = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool CtsHolding => InnerClient.CtsHolding;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool DiscardNull { get => InnerClient.DiscardNull; set => InnerClient.DiscardNull = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int DataBits { get => InnerClient.DataBits; set => InnerClient.DataBits = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool DsrHolding => InnerClient.DsrHolding;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string NewLine { get => InnerClient.NewLine; set => InnerClient.NewLine = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override int ReceiveBufferSize { get => InnerClient.ReadBufferSize; set => InnerClient.ReadBufferSize = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public byte ParityReplace { get => InnerClient.ParityReplace; set => InnerClient.ParityReplace = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string PortName { get => InnerClient.PortName; set => InnerClient.PortName = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool CDHolding => InnerClient.CDHolding;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int ReadTimeout { get => InnerClient.ReadTimeout; set => InnerClient.ReadTimeout = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int ReceivedBytesThreshold { get; set; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool RtsEnable { get => InnerClient.RtsEnable; set => InnerClient.RtsEnable = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public StopBits StopBits { get => InnerClient.StopBits; set => InnerClient.StopBits = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override int SendBufferSize { get => InnerClient.WriteBufferSize; set => InnerClient.WriteBufferSize = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int WriteTimeout { get => InnerClient.WriteTimeout; set => InnerClient.WriteTimeout = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Parity Parity { get => InnerClient.Parity; set => InnerClient.Parity = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int BytesToWrite => InnerClient.BytesToWrite;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int BaudRate { get => InnerClient.BaudRate; set => InnerClient.BaudRate = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool BreakState { get => InnerClient.BreakState; set => InnerClient.BreakState = value; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Stream BaseStream => InnerClient.BaseStream;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int BytesToRead => InnerClient.BytesToRead;
+
+        /// <summary>
+        /// <inheritdoc/>
         /// </summary>
         public void DiscardInBuffer()
         {
-            innerClient.DiscardInBuffer();
+            InnerClient.DiscardInBuffer();
         }
 
         /// <summary>
-        /// 丢弃来自串行驱动程序的传输缓冲区的数据
+        /// <inheritdoc/>
         /// </summary>
         public void DiscardOutBuffer()
         {
-            innerClient.DiscardOutBuffer();
+            InnerClient.DiscardOutBuffer();
         }
     }
 }
