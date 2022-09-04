6a4e8a86fd18847f79f8beb8a41bf1c5c2af85f3
Guijie Lee <landriesnidis@yeah.net>
2022-09-05 00:10:23 +0800

    增加Modbus命令枚举的Description

--- STTech.BytesIO.Modbus/ModbusCommand.cs
+++ STTech.BytesIO.Modbus/ModbusCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace STTech.BytesIO.Modbus
 {
@@ -10,42 +11,50 @@ namespace STTech.BytesIO.Modbus
         /// <summary>
         /// 读线圈寄存器
         /// </summary>
+        [Description("读线圈寄存器")]
         ReadCoilRegister = 0x01,
 
         /// <summary>
         /// 读离散输入寄存器
         /// </summary>
+        [Description("读离散输入寄存器")] 
         ReadDiscreteInputRegister = 0x02,
 
 
         /// <summary>
         /// 读保持寄存器
         /// </summary>
+        [Description("读保持寄存器")] 
         ReadHoldRegister = 0x03,
 
         /// <summary>
         ///  读输入寄存器
         /// </summary>
+        [Description("读输入寄存器")] 
         ReadInputRegister = 0x04,
 
         /// <summary>
         /// 写单个线圈寄存器
         /// </summary>
+        [Description("写单个线圈寄存器")] 
         WriteSingleCoilRegister = 0x05,
 
         /// <summary>
         /// 写单个保持寄存器
         /// </summary>
+        [Description("写单个保持寄存器")] 
         WriteSingleHoldRegister = 0x06,
 
         /// <summary>
         /// 写多个线圈寄存器
         /// </summary>
+        [Description("写多个线圈寄存器")] 
         WriteMultipleCoilRegisters = 0x0F,
 
         /// <summary>
         /// 写多个保持寄存器
         /// </summary>
+        [Description("写多个保持寄存器")] 
         WriteMultipleHoldRegisters = 0x10,
     }
 
