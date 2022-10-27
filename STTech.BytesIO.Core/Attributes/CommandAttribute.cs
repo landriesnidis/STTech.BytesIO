1b487a8bd1ccc7c1bcd34d1d307bd27fe8dfe232
Guijie Lee <landriesnidis@yeah.net>
2022-10-28 00:21:48 +0800

    暂取消Attributes相关的开发

--- STTech.BytesIO.Core/Attributes/CommandAttribute.cs
+++ STTech.BytesIO.Core/Attributes/CommandAttribute.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace STTech.BytesIO.Core.Attributes
-{
-    /// <summary>
-    /// 命令
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public class CommandAttribute: Attribute
-    {
-        /// <summary>
-        /// 显示名
-        /// </summary>
-        public string DisplayName { get; set; }
+//namespace STTech.BytesIO.Core.Attributes
+//{
+//    /// <summary>
+//    /// 命令
+//    /// </summary>
+//    [AttributeUsage(AttributeTargets.Method)]
+//    public class CommandAttribute: Attribute
+//    {
+//        /// <summary>
+//        /// 显示名
+//        /// </summary>
+//        public string DisplayName { get; set; }
 
-        /// <summary>
-        /// 命令
-        /// </summary>
-        /// <param name="displayName">显示名称</param>
-        public CommandAttribute(string displayName)
-        {
-            DisplayName = displayName;
-        }
-    }
-}
+//        /// <summary>
+//        /// 命令
+//        /// </summary>
+//        /// <param name="displayName">显示名称</param>
+//        public CommandAttribute(string displayName)
+//        {
+//            DisplayName = displayName;
+//        }
+//    }
+//}
