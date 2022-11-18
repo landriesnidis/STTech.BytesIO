01baa6d9988189645e327ee7d7a7c1a327bfb46c
Guijie Lee <landriesnidis@yeah.net>
2022-11-19 02:52:11 +0800

    将Unpacker<T>移到单独的源码文件

---
+++ STTech.BytesIO.Core/Components/Unpacker.T.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STTech.BytesIO.Core.Component
+{
+    /// <summary>
+    /// 泛型解包器
+    /// 继承字节数组的解包器，使用泛型解包器可以直接获取到泛型对象
+    /// 注意：基于Response对象必须拥有一个带参的构造函数且参数类型为IEnumerable&lt;byte&gt;
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Unpacker<T> : Unpacker where T : Response
+    {
+        /// <summary>
+        /// 当解析出结果时发生
+        /// </summary>
+        public new event EventHandler<DataParsedEventArgs<T>> OnDataParsed;
+
+        /// <summary>
+        /// 所属的客户端
+        /// </summary>
+        public BytesClient Client { get; internal set; }
+
+        /// <summary>
+        /// 计算包长度回调方法
+        /// </summary>
+        private readonly Func<IEnumerable<byte>, int> _calculatePacketLengthHandler;
+
+        /// <summary>
+        /// 构造解包器
+        /// </summary>
+        public Unpacker(BytesClient client, Func<IEnumerable<byte>, int> calculatePacketLengthHandler = null)
+        {
+            base.OnDataParsed += Unpacker_OnDataParsed;
+            Client = client;
+            _calculatePacketLengthHandler = calculatePacketLengthHandler;
+        }
+
+        private void Unpacker_OnDataParsed(object sender, DataParsedEventArgs e)
+        {
+            // 反射构造对象
+            // 基于Response对象必须拥有一个带参的构造函数且参数类型为IEnumerable<byte>
+            T data = Activator.CreateInstance(typeof(T), new object[] { e.Data }) as T;
+
+            // 反射执行回调
+            OnDataParsed?.Invoke(this, new DataParsedEventArgs<T>(data));
+        }
+
+        /// <summary>
+        /// 计算数据包长度的处理程序
+        /// 输入当前缓存的数据
+        /// 输出第一个数据包的长度，若暂无法判断数据包总长度，可返回0
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        protected override int CalculatePacketLength(IEnumerable<byte> bytes)
+        {
+            return _calculatePacketLengthHandler(bytes);
+        }
+    }
+
+    /// <summary>
+    /// 解析出结果(bytes)的事件参数
+    /// </summary>
+    public class DataParsedEventArgs : DataParsedEventArgs<IEnumerable<byte>>
+    {
+        public DataParsedEventArgs(IEnumerable<byte> data) : base(data) { }
+    }
+
+    /// <summary>
+    /// 解析出结果的事件参数
+    /// </summary>
+    public class DataParsedEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// 解析出的数据
+        /// </summary>
+        public T Data { get; }
+
+        public DataParsedEventArgs(T data)
+        {
+            Data = data;
+        }
+    }
+}
--- STTech.BytesIO.Core/Components/Unpacker.cs
+++ STTech.BytesIO.Core/Components/Unpacker.cs
@@ -123,15 +123,15 @@ namespace STTech.BytesIO.Core.Component
                             if(index == -1)
                             {
                                 // 未找到起始位则清空数据
-                            ErrorOccurHandler?.Invoke(ErrorCode.StartMarkNotMatch);
-                            _UnprocessedDataCache = null;
-                            return;
-                        }
+                                ErrorOccurHandler?.Invoke(ErrorCode.StartMarkNotMatch);
+                                _UnprocessedDataCache = null;
+                                return;
+                            }
                             else
                             {
                                 // 若找到起始位则认为缓冲区前段混入脏数据，跳过这些字节
                                 _UnprocessedDataCache = _UnprocessedDataCache.Skip(index);
-                    }
+                            }
                         }
                     }
 
@@ -179,85 +179,5 @@ namespace STTech.BytesIO.Core.Component
         }
     }
 
-    /// <summary>
-    /// 泛型解包器
-    /// 继承字节数组的解包器，使用泛型解包器可以直接获取到泛型对象
-    /// 注意：基于Response对象必须拥有一个带参的构造函数且参数类型为IEnumerable&lt;byte&gt;
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class Unpacker<T> : Unpacker where T : Response
-    {
-        /// <summary>
-        /// 当解析出结果时发生
-        /// </summary>
-        public new event EventHandler<DataParsedEventArgs<T>> OnDataParsed;
 
-        /// <summary>
-        /// 所属的客户端
-        /// </summary>
-        public BytesClient Client { get; internal set; }
-
-        /// <summary>
-        /// 计算包长度回调方法
-        /// </summary>
-        private readonly Func<IEnumerable<byte>, int> _calculatePacketLengthHandler;
-
-        /// <summary>
-        /// 构造解包器
-        /// </summary>
-        public Unpacker(BytesClient client, Func<IEnumerable<byte>, int> calculatePacketLengthHandler = null)
-        {
-            base.OnDataParsed += Unpacker_OnDataParsed;
-            Client = client;
-            _calculatePacketLengthHandler = calculatePacketLengthHandler;
-        }
-
-        private void Unpacker_OnDataParsed(object sender, DataParsedEventArgs e)
-        {
-            // 反射构造对象
-            // 基于Response对象必须拥有一个带参的构造函数且参数类型为IEnumerable<byte>
-            T data = Activator.CreateInstance(typeof(T), new object[] { e.Data }) as T;
-
-            // 反射执行回调
-            OnDataParsed?.Invoke(this, new DataParsedEventArgs<T>(data));
-        }
-
-        /// <summary>
-        /// 计算数据包长度的处理程序
-        /// 输入当前缓存的数据
-        /// 输出第一个数据包的长度，若暂无法判断数据包总长度，可返回0
-        /// </summary>
-        /// <param name="bytes"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        protected override int CalculatePacketLength(IEnumerable<byte> bytes)
-        {
-            return _calculatePacketLengthHandler(bytes);
-        }
-    }
-
-
-    /// <summary>
-    /// 解析出结果(bytes)的事件参数
-    /// </summary>
-    public class DataParsedEventArgs : DataParsedEventArgs<IEnumerable<byte>>
-    {
-        public DataParsedEventArgs(IEnumerable<byte> data) : base(data) { }
-    }
-
-    /// <summary>
-    /// 解析出结果的事件参数
-    /// </summary>
-    public class DataParsedEventArgs<T> : EventArgs
-    {
-        /// <summary>
-        /// 解析出的数据
-        /// </summary>
-        public T Data { get; }
-
-        public DataParsedEventArgs(T data)
-        {
-            Data = data;
-        }
-    }
 }
