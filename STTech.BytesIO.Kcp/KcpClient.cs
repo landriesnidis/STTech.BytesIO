a494dae39c40e5e19f1870171b49ec3e5afb0bac
Guijie Lee <landriesnidis@yeah.net>
2023-04-27 00:21:28 +0800

    新增Kcp客户端的实现

---
+++ STTech.BytesIO.Kcp/KcpClient.cs
@@ -0,0 +1,140 @@
+﻿using KcpSharp;
+using STTech.BytesIO.Core;
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace STTech.BytesIO.Kcp
+{
+    public interface IKcpClient
+    {
+        /// <summary>
+        ///远程主机网络地址
+        /// </summary>
+        string Host { get; set; }
+
+        /// <summary>
+        /// 远程主机端口号
+        /// </summary>
+        int Port { get; set; }
+        int ConversationId { get; set; }
+    }
+    public partial class KcpClient : BytesClient
+    {
+
+        /// <summary>
+        /// 内部KCP连接
+        /// </summary>
+        protected KcpConversation InnerClient { get; set; }
+
+        public override ConnectResult Connect(ConnectArgument argument = null)
+        {
+            try
+            {
+                using var socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+
+                EndPoint endPoint = new IPEndPoint(IPAddress.Parse(Host), Port);
+
+                socket.Connect(endPoint);
+                using var transport = KcpSocketTransport.CreateConversation(socket, endPoint, ConversationId, null);
+                transport.Start();
+
+                InnerClient = transport.Connection;
+
+                return new ConnectResult();
+            }
+            catch (Exception ex)
+            {
+                return new ConnectResult(ConnectErrorCode.Error, ex);
+            }
+        }
+
+        public override DisconnectResult Disconnect(DisconnectArgument argument = null)
+        {
+            if (InnerClient == null)
+            {
+                return new DisconnectResult(DisconnectErrorCode.NoConnection);
+            }
+
+            try
+            {
+                InnerClient.SetTransportClosed();
+                return new DisconnectResult();
+            }
+            catch (Exception ex)
+            {
+                return new DisconnectResult(DisconnectErrorCode.Error, ex);
+            }
+            finally
+            {
+                InnerClient = null;
+            }
+        }
+
+        public override void Dispose()
+        {
+            Disconnect();
+            InnerClient?.Dispose();
+        }
+
+        protected override void ReceiveDataCompletedHandle()
+        {
+            Disconnect(new DisconnectArgument(DisconnectionReasonCode.Passive));
+        }
+
+        protected override async void ReceiveDataHandle()
+        {
+            try
+            {
+                byte[] buffer = new byte[MaxMessageSize];
+                while (IsConnected)
+                {
+                    var result = await InnerClient.ReceiveAsync(buffer, CancellationToken.None);
+
+                    if (result.TransportClosed)
+                    {
+                        break;
+                    }
+
+                    var data = buffer.AsMemory(0, result.BytesReceived).ToArray();
+                    InvokeDataReceivedEventCallback(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                // 回调异常事件
+                RaiseExceptionOccurs(this, new ExceptionOccursEventArgs(ex));
+
+                return;
+            }
+        }
+
+        protected override async void SendHandler(byte[] data)
+        {
+            await InnerClient.SendAsync(data);
+        }
+    }
+
+    public partial class KcpClient : IKcpClient
+    {
+        public int ConversationId { get; set; } = 1;
+        public string Host { get; set; } = "127.0.0.1";
+        public int Port { get; set; } = 5000;
+
+        private const int MaxMessageSize = 16384; // limit of the maximum message size.
+
+        public override int ReceiveBufferSize
+        {
+            get => base.ReceiveBufferSize;
+            set
+            {
+                if (value < 50 || value > MaxMessageSize)
+                {
+                    throw new ArgumentException($"KCP ReceiveBufferSize: [50,{MaxMessageSize}].");
+                }
+                base.ReceiveBufferSize = value;
+            }
+        }
+    }
+}
