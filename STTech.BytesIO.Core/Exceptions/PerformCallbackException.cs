5417500733f23437587253c329f7e1f50edf500a
Guijie Lee <landriesnidis@yeah.net>
2023-11-17 23:58:33 +0800

    新增细分的异常子类

--- STTech.BytesIO.Core/Exceptions/PerformCallbackException.cs
+++ STTech.BytesIO.Core/Exceptions/PerformCallbackException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace STTech.BytesIO.Core.Exceptions
 {
-    public class PerformCallbackException : Exception
+    public class PerformCallbackException : BytesIOException
     {
         private const string errorMessage = "An exception occurred while the callback method was executing.";
         public PerformCallbackException(Exception ex) : base(errorMessage, ex) { }
--- STTech.BytesIO.Core/Exceptions/ReplyConversionException.cs
+++ STTech.BytesIO.Core/Exceptions/ReplyConversionException.cs
@@ -5,7 +5,7 @@ namespace STTech.BytesIO.Core.Exceptions
     /// <summary>
     /// Reply格式转换时发生的异常
     /// </summary>
-    public class ReplyConversionException : Exception
+    public class ReplyConversionException : BytesIOException
     {
         public ReplyConversionException(string message, Exception ex) : base(message, ex) { }
     }
