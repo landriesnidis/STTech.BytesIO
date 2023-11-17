82f9305212552b84bec24625cf076fdb23a4ede5
Guijie Lee <landriesnidis@yeah.net>
2023-11-17 23:58:02 +0800

    新增异常基类

---
+++ STTech.BytesIO.Core/Exceptions/BytesIOException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace STTech.BytesIO.Core.Exceptions
+{
+    public class BytesIOException : Exception
+    {
+        public BytesIOException(string errorMessage,Exception ex) : base(errorMessage, ex) { }
+    }
+}
