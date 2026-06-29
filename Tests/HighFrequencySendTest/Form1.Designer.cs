cbe2c4b5c6a1c49f3a58897c98f587a65064fe33
GuijieLi <landriesnidis@yeah.net>
2026-06-29 15:20:27 +0800

    test: 新增高频发送测试程序 HighFrequencySendTest

---
+++ Tests/HighFrequencySendTest/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace HighFrequencySendTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnTestTcp = new System.Windows.Forms.ToolStripButton();
+            this.btnTestSerialPort = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pgClient = new System.Windows.Forms.PropertyGrid();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pgTimer = new System.Windows.Forms.PropertyGrid();
+            this.timerSend = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.btnConnect = new System.Windows.Forms.ToolStripButton();
+            this.btnDisconnect = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.labClientStatus = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnTestTcp,
+            this.btnTestSerialPort});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnTestTcp
+            // 
+            this.btnTestTcp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnTestTcp.Image = ((System.Drawing.Image)(resources.GetObject("btnTestTcp.Image")));
+            this.btnTestTcp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTestTcp.Name = "btnTestTcp";
+            this.btnTestTcp.Size = new System.Drawing.Size(34, 22);
+            this.btnTestTcp.Text = "TCP";
+            this.btnTestTcp.Click += new System.EventHandler(this.btnTestTcp_Click);
+            // 
+            // btnTestSerialPort
+            // 
+            this.btnTestSerialPort.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnTestSerialPort.Image = ((System.Drawing.Image)(resources.GetObject("btnTestSerialPort.Image")));
+            this.btnTestSerialPort.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTestSerialPort.Name = "btnTestSerialPort";
+            this.btnTestSerialPort.Size = new System.Drawing.Size(68, 22);
+            this.btnTestSerialPort.Text = "SerialPort";
+            this.btnTestSerialPort.Click += new System.EventHandler(this.btnTestSerialPort_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pgClient);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 400);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client Settings";
+            // 
+            // pgClient
+            // 
+            this.pgClient.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgClient.Location = new System.Drawing.Point(3, 17);
+            this.pgClient.Name = "pgClient";
+            this.pgClient.Size = new System.Drawing.Size(194, 380);
+            this.pgClient.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pgTimer);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Location = new System.Drawing.Point(200, 50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 400);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Timer Settings";
+            // 
+            // pgTimer
+            // 
+            this.pgTimer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgTimer.Location = new System.Drawing.Point(3, 17);
+            this.pgTimer.Name = "pgTimer";
+            this.pgTimer.Size = new System.Drawing.Size(194, 380);
+            this.pgTimer.TabIndex = 1;
+            // 
+            // timerSend
+            // 
+            this.timerSend.Tick += new System.EventHandler(this.timerSend_Tick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(400, 50);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(400, 400);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Operation";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Controls.Add(this.btnStop);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 17);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(394, 380);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(3, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(129, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(120, 40);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnConnect,
+            this.btnDisconnect,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.labClientStatus});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip2.TabIndex = 5;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnConnect.Image = ((System.Drawing.Image)(resources.GetObject("btnConnect.Image")));
+            this.btnConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(59, 22);
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnDisconnect.Image = ((System.Drawing.Image)(resources.GetObject("btnDisconnect.Image")));
+            this.btnDisconnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 22);
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(46, 22);
+            this.toolStripLabel1.Text = "Status:";
+            // 
+            // labClientStatus
+            // 
+            this.labClientStatus.Name = "labClientStatus";
+            this.labClientStatus.Size = new System.Drawing.Size(39, 22);
+            this.labClientStatus.Text = "NULL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnTestTcp;
+        private System.Windows.Forms.ToolStripButton btnTestSerialPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PropertyGrid pgClient;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PropertyGrid pgTimer;
+        private System.Windows.Forms.Timer timerSend;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton btnConnect;
+        private System.Windows.Forms.ToolStripButton btnDisconnect;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel labClientStatus;
+    }
+}
+
---
+++ Tests/HighFrequencySendTest/Form1.cs
@@ -0,0 +1,106 @@
+﻿using STTech.BytesIO.Core;
+using STTech.BytesIO.Serial;
+using STTech.BytesIO.Tcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HighFrequencySendTest
+{
+    public partial class Form1 : Form
+    {
+        private BytesClient client;
+        private byte[] data = new byte[] { 0x55, 0x0A, 0x00, 0xAA };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pgTimer.SelectedObject = timerSend;
+        }
+
+        private void SetClient(BytesClient client)
+        {
+            if (this.client != null)
+            {
+                if (this.client.IsConnected)
+                {
+                    this.client.Disconnect();
+                }
+                this.client.OnConnectedSuccessfully -= Client_OnConnectedSuccessfully;
+                this.client.OnDisconnected -= Client_OnDisconnected;
+                this.client.OnConnectionFailed -= Client_OnConnectionFailed;
+                this.client.Dispose();
+            }
+
+            this.client = client;
+            this.client.OnConnectedSuccessfully += Client_OnConnectedSuccessfully;
+            this.client.OnDisconnected += Client_OnDisconnected;
+            this.client.OnConnectionFailed += Client_OnConnectionFailed;
+
+            pgClient.SelectedObject = null;
+            pgClient.SelectedObject = client;
+        }
+
+        private void Client_OnConnectionFailed(object sender, ConnectionFailedEventArgs e) => UpdateClientStatus("Failed");
+        private void Client_OnDisconnected(object sender, DisconnectedEventArgs e) => UpdateClientStatus($"Disconnected({e.ReasonCode})");
+        private void Client_OnConnectedSuccessfully(object sender, ConnectedSuccessfullyEventArgs e) => UpdateClientStatus("Connected");
+
+        private void UpdateClientStatus(string str)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateClientStatus), str);
+                return;
+            }
+            labClientStatus.Text = str;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerSend.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timerSend.Enabled = false;
+        }
+
+        private void btnTestTcp_Click(object sender, EventArgs e)
+        {
+            SetClient(new TcpClient());
+        }
+
+        private void btnTestSerialPort_Click(object sender, EventArgs e)
+        {
+            SetClient(new SerialClient());
+        }
+
+        private void timerSend_Tick(object sender, EventArgs e)
+        {
+            if (client == null || !client.IsConnected)
+            {
+                timerSend.Enabled = false;
+                return;
+            }
+
+            client.SendAsync(data);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client?.Connect();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            client?.Disconnect();
+        }
+    }
+}
